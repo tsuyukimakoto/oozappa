08869fc23e943468b6eeccb56fbebc6a52efbaec
makoto tsuyuki <mtsuyuki@gmail.com>
2017-12-16 14:13:30 +0900

    bump version

--- oozappa/__init__.py
+++ oozappa/__init__.py
@@ -12,7 +12,7 @@ logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(levelname)s %(message)s')
 logger = logging.getLogger(__name__)
 
-version_info = (0, 9, 0)
+version_info = (0, 9, 1)
 __version__ = ".".join([str(v) for v in version_info])
 
 
